b44bb129efc4a1e238aea3cd8e6b2449d88114dc
Sylicium <96630108+Sylicium@users.noreply.github.com>
2022-08-18 02:06:13 +0200

    Updated personnal module logger.js to 3.1.1
    Updated personnal module logger.js from 2.1.2 to 3.1.1

--- localModules/logger.js
+++ localModules/logger.js
@@ -1,6 +1,6 @@
 
 /**
- * @version 2.1.2 // 24/04/2022
+ * @version 3.1.1 // 28/07/2022
  * @author Sylicium
  * @description Module global qui servira à log pour tous les fichiers au lieu de faire toujours un console.log
  *
@@ -13,9 +13,12 @@ let config = require("../config")
 
 let configuration = {
     coloration: "colored", // normal: erreur tout en rouge, warn tout en orange etc.. | colored couleurs différentes pour la ligne de log pour la date, heure etc..
-    logOnStart: false
+    logOnStart: false,
+    ReplitName: "ReferencementDBG",
+    workDirectoryFullPath: "c:\\Users\\Sylicium\\Documents\\devlopement\\Projets\\#0006 Discord bot settings API\\v2\\Discord-bot-settings-API",
+    workDirectory: "\\ReferencementDBG",
+    replitMode: false
 }
-
 function formatDate(timestamp, format) {
     /*
     YYYY: year
@@ -28,7 +31,7 @@ function formatDate(timestamp, format) {
     ms: miliseconds
     */
     let la_date = new Date(timestamp)
-    function formatThis(thing, length=2) {
+    function formatThis(thing, length = 2) {
         return `0000${thing}`.substr(-length)
     }
 
@@ -42,17 +45,17 @@ function formatDate(timestamp, format) {
             "samedi",
             "dimanche"
         ]
-        return list[la_date.getDay()-1]
+        return list[la_date.getDay() - 1]
     }
 
-    let return_string = format.replace("YYYY", la_date.getFullYear()).replace("MM", formatThis(la_date.getMonth()+1)).replace("DDDDD", getDayName()).replace("DD", formatThis(la_date.getDate())).replace("hh", formatThis(la_date.getHours())).replace("mm", formatThis(la_date.getMinutes())).replace("ss", formatThis(la_date.getSeconds())).replace("ms", formatThis(la_date.getMilliseconds(),3))
+    let return_string = format.replace("YYYY", la_date.getFullYear()).replace("MM", formatThis(la_date.getMonth() + 1)).replace("DDDDD", getDayName()).replace("DD", formatThis(la_date.getDate())).replace("hh", formatThis(la_date.getHours())).replace("mm", formatThis(la_date.getMinutes())).replace("ss", formatThis(la_date.getSeconds())).replace("ms", formatThis(la_date.getMilliseconds(), 3))
 
     return return_string
 }
 
-function getDateString(format, timestamp=null) {
+function getDateString(format, timestamp = null) {
     let d = new Date()//.toLocaleString("fr-FR", { timeZone: "Europe/Paris" });
-    if(timestamp) {
+    if (timestamp) {
         d = new Date(timestamp)
     }
     return formatDate(d.getTime(), (format || "DD/MM/YYYY - hh:mm:ss"))
@@ -61,11 +64,11 @@ function getDateString(format, timestamp=null) {
 
 function colorizeThisType(type, text) {
     type = type.toUpperCase()
-    if(type == "ERROR") return clc.red(text)
-    if(type == "WARN") return clc.yellow(text)
-    if(type == "DEBUG") return clc.magentaBright(text)
-    if(type == "INFO") return clc.blueBright(text)
-    if(type == "LOG") return clc.whiteBright(text)
+    if (type == "ERROR") return clc.red(text)
+    if (type == "WARN") return clc.yellow(text)
+    if (type == "DEBUG") return clc.magentaBright(text)
+    if (type == "INFO") return clc.blueBright(text)
+    if (type == "LOG") return clc.whiteBright(text)
     return clc.white(text)
 }
 
@@ -123,26 +126,36 @@ let colors = {
 class Logger {
     constructor(logName) {
 
-        function _getCallerFile(){
+        function _getCallerFile() {
             const err = new Error();
-        
+
             Error.prepareStackTrace = (_, stack) => stack;
-        
+
             const stack = err.stack;
-        
+
             Error.prepareStackTrace = undefined;
 
             let fullFilePath = stack[2].getFileName()
-            
+
             let temp = stack[2].getFileName().split("\\")
-            let _a_fileName = temp[temp.length-1].split(".")
+            let _a_fileName = temp[temp.length - 1].split(".")
             _a_fileName.pop()
             let fileName = _a_fileName.join(".")
-            
+
+            let filePath = fullFilePath
+            // let fileName = fileName
+            let fullFileName = temp[temp.length - 1]
+
+            if (configuration.replitMode) {
+                filePath = fullFilePath.replace(`/home/runner/${configuration.ReplitName}/`, ""),
+                    fileName = fileName.replace(`/home/runner/${configuration.ReplitName}/`, ""),
+                    fullFileName = fullFileName.replace(`/home/runner/${configuration.ReplitName}/`, "")
+            }
+
             return {
                 filePath: fullFilePath,
                 fileName: fileName,
-                fullFileName: temp[temp.length-1]
+                fullFileName: fullFileName
             }
         }
         let fileInfos = _getCallerFile()
@@ -150,16 +163,18 @@ class Logger {
         /*if(!logName) {
             
         }*/
+        //console.log("fileInfos:",fileInfos)
+        fileInfos.fileName = fileInfos.fileName
         let logFileName = fileInfos.fileName
 
         this.writeLog = this.writeLog
         this.callerFile = fileInfos
         this.logFile = {
-            filePath: `./logs/${fileInfos.fileName}.log`,
+            filePath: `${configuration.replitMode ? `/home/runner/${configuration.ReplitName}/logs/` : "./logs/"}${fileInfos.fileName}.log`,
             fileName: logFileName,
             logName: (!!logName ? logName : null)
         }
-        
+
         /*
         this.writeAllLogs = (type, a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z) => {
             
@@ -184,27 +199,30 @@ class Logger {
         }
         */
 
-        if(!fs.existsSync(this.logFile.filePath)) {
-            fs.writeFileSync(this.logFile.filePath,`File created on ${getDateString("DD/MM/YYYY at hh:mm:ss.ms")}\n\n`)
+        if (!fs.existsSync(this.logFile.filePath)) {
+            fs.writeFileSync(this.logFile.filePath, `File created on ${getDateString("DD/MM/YYYY at hh:mm:ss.ms")}\n\n`)
         }
-        
+
         this._writeLogLine = (type, ...args) => {
 
             let callerLine;
             try {
-                let s = (new Error()).stack.split("\n")[3].split("\\")
-                callerLine = "\\" + s[s.length-2] + "\\" + s[s.length-1]
-                if(callerLine.substr(-1) == ")") callerLine = callerLine.substr(0, callerLine.length-1)
-            } catch(e) {
+                let splitOnChar = "\\"
+                let s = ((new Error()).stack)
+                if (s.split("/").length > s.split("\\").length) splitOnChar = "/"
+                let s_2 = s.split("\n")[3].split(splitOnChar)
+                callerLine = splitOnChar + s_2[s_2.length - 2] + splitOnChar + s_2[s_2.length - 1]
+                if (callerLine.substr(-1) == ")") callerLine = callerLine.substr(0, callerLine.length - 1)
+            } catch (e) {
                 callerLine = `!${this.logFile.logName ? `n> ${this.logFile.logName}` : this.callerFile.fullFileName}`
             }
             //console.log(` >>${callerLine}<<`)
 
             ////////// à remove sur la prod //////////
-            if(callerLine.startsWith("\\Serveur bancaire")) callerLine = callerLine.replace("\\Serveur bancaire","<root>")
+            if (callerLine.startsWith(configuration.workDirectory)) callerLine = callerLine.replace(configuration.workDirectory, "<root>")
 
 
-            if(!callerLine) callerLine = `!${this.logFile.logName ? `n> ${this.logFile.logName}` : this.callerFile.fullFileName}`
+            if (!callerLine) callerLine = `!${this.logFile.logName ? `n> ${this.logFile.logName}` : this.callerFile.fullFileName}`
 
 
             type = type.toUpperCase()
@@ -213,40 +231,40 @@ class Logger {
 
             //text = JSON.stringify(a),JSON.stringify(b),JSON.stringify(c),JSON.stringify(d),JSON.stringify(e),JSON.stringify(f),JSON.stringify(g),JSON.stringify(h),JSON.stringify(i),JSON.stringify(j),JSON.stringify(k),JSON.stringify(l),JSON.stringify(m),JSON.stringify(n),JSON.stringify(o),JSON.stringify(p),JSON.stringify(q),JSON.stringify(r),JSON.stringify(s),JSON.stringify(t),JSON.stringify(u),JSON.stringify(v),JSON.stringify(w),JSON.stringify(x),JSON.stringify(y),JSON.stringify(z)
 
-            let log_text = `[${getDateString("DD/MM/YYYY - hh:mm:ss.ms",date_timestamp)} ${type}] (${callerLine}): ${args}\n`
+            let log_text = `[${getDateString("DD/MM/YYYY - hh:mm:ss.ms", date_timestamp)} ${type}] (${callerLine}): ${args}\n`
             //console.log(`[${date} ${type.toUpperCase()}] (${callerLine}): ${text}`)
 
-            if(configuration.coloration == "colored") {
+            if (configuration.coloration == "colored") {
                 let dt = {
-                    days: getDateString("DD",date_timestamp),
-                    months: getDateString("MM",date_timestamp),
-                    year: getDateString("YYYY",date_timestamp),
-                    hours: getDateString("hh",date_timestamp),
-                    minutes: getDateString("mm",date_timestamp),
-                    seconds: getDateString("ss",date_timestamp),
-                    milliseconds: getDateString("ms",date_timestamp),
-                    allTime: getDateString("hh:mm:ss.ms",date_timestamp)
+                    days: getDateString("DD", date_timestamp),
+                    months: getDateString("MM", date_timestamp),
+                    year: getDateString("YYYY", date_timestamp),
+                    hours: getDateString("hh", date_timestamp),
+                    minutes: getDateString("mm", date_timestamp),
+                    seconds: getDateString("ss", date_timestamp),
+                    milliseconds: getDateString("ms", date_timestamp),
+                    allTime: getDateString("hh:mm:ss.ms", date_timestamp)
                 }
                 let colorized_date = `${clc.cyan(dt.days)}${clc.white("/")}${clc.cyan(dt.months)}${clc.white("/")}${clc.cyan(dt.year)} - `
                 colorized_date += `${clc.green(`${dt.allTime}`)}`
 
                 // let text_to_log = `[${colorized_date} ${colorizeThisType(type,type)}] (${clc.blue(callerLine)}): ${colorizeThisType(type,text)}`
                 // console.log(text_to_log)
-                let pretext_to_log = `[${colorized_date} ${colorizeThisType(type,type)}] (${clc.blue(callerLine)}):`
+                let pretext_to_log = `[${colorized_date} ${colorizeThisType(type, type)}] (${clc.blue(callerLine)}):`
                 console.log(pretext_to_log, ...args)
             } else {
-                let date = getDateString("DD/MM/YYYY - hh:mm:ss.ms",date_timestamp)
+                let date = getDateString("DD/MM/YYYY - hh:mm:ss.ms", date_timestamp)
                 let text_to_log = `[${date} ${type}] (${callerLine}): ${text}`
-                if(type == "ERROR") console.log(clc.redBright(text_to_log))
-                else if(type == "WARN") console.log(clc.yellow(text_to_log))
-                else if(type == "DEBUG") console.log(clc.magentaBright(text_to_log))
-                else if(type == "INFO") console.log(clc.blueBright(text_to_log))
-                else if(type == "LOG") console.log(clc.whiteBright(text_to_log))
-                else {console.log(clc.white(text_to_log))}
+                if (type == "ERROR") console.log(clc.redBright(text_to_log))
+                else if (type == "WARN") console.log(clc.yellow(text_to_log))
+                else if (type == "DEBUG") console.log(clc.magentaBright(text_to_log))
+                else if (type == "INFO") console.log(clc.blueBright(text_to_log))
+                else if (type == "LOG") console.log(clc.whiteBright(text_to_log))
+                else { console.log(clc.white(text_to_log)) }
             }
 
-            
-            fs.appendFileSync(this.logFile.filePath,log_text, 'utf8');
+
+            fs.appendFileSync(this.logFile.filePath, log_text, 'utf8');
         }
 
         /*
@@ -270,10 +288,11 @@ class Logger {
         }
         */
         //this.writeLog = writeLog
-        
-        if(configuration.logOnStart) this._writeLogLine("info",`Starting logger for file '${this.callerFile.filePath.replace("c:\\Users\\Sylicium\\Documents\\DirtyBiologystan\\Projets\\Monnaie\\Serveur bancaire","<root>\\")}'`)
+
+        if (configuration.logOnStart) this._writeLogLine("info", `Starting logger for file '${this.callerFile.filePath.replace(configuration.workDirectoryFullPath, "<root>>\\")}'`)
 
     }
+
 
     log = (...args) => {
         this._writeLogLine("LOG", ...args)
@@ -294,9 +313,4 @@ class Logger {
 }
 
 module.exports = Logger
-
-
-
-
-
 
